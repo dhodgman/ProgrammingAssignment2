eb379808b59300281dc54da8135a83ed30f2a8ec
David Hodgman <david.hodgman@corrs.com.au>
2015-02-18 14:09:11 +1100

    Improved comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,7 @@
 ## The makeCacheMatrix function returns an object (a list) that has the
-## ability to return the inverse of the given matrix.  
-##
-## Calling the cacheSolve(x) function passing the object returned from 
-## makeCacheMatrix will return the inverse of the given matrix.  Upon the 
-## first call to cacheSolve, the inverted matrix will be cached.  On 
-## subsequent calls, the previously inverted matrix is returned from the 
-## cache thus improving performance.  
-##
-## If the supplied matrix is replaced with a call to the set(y) function,
-## the inverse of the new matrix will be calculated and cached on the 
-## next call to cacheSolve.
+## ability to return the inverse of the given matrix 'x'.  
+## Call the cacheSolve function passing the object returned by makeCacheMatrix
+## in order to access the inverse of the matrix 'x'.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -26,7 +18,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Return a matrix that is the inverse of 'x'
+## Calling the cacheSolve(x) function passing the object returned from 
+## makeCacheMatrix will return the inverse of the given matrix 'x'.  
+## Upon the first call to cacheSolve, the inverted matrix will be cached.  
+## On subsequent calls, the previously inverted matrix is returned from 
+## the cache thus improving performance.  
+##
+## If the supplied matrix is replaced with a call to the set(y) function,
+## the inverse of the new matrix will be calculated and cached on the 
+## next call to cacheSolve.
+
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
